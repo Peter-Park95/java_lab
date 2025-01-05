8b28c39a94a2e7d233938eb3fc86d15492072480
Peter <baaams.park@gmail.com>
2025-01-05 20:45:52 +0900

    250105 commit

--- src/chapter02/_01_Operator1.java
+++ src/chapter02/_01_Operator1.java
@@ -1,8 +1,8 @@
 package chapter02;
 
 public class _01_Operator1 {
-    public static void main(String[] args) {
-        int height = 115;
-        System.out.println((height >= 120) ? ("키가 " + height + "cm 이므로 탑승 가능합니다") : ("키가 " + height + "cm 이므로 탑승 불가능합니다"));
+    public static void main(String[] args){
+        String s = "I like Java and Python and C.";
+        System.out.println(s.replace(" and", ","));
     }
 }
---
+++ src/chapter02/_02_Quiz.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class _02_Quiz {
+    public static void main(String[] args) {
+        String id = "950906-1188725";
+        String change_id = id.substring(0,8);
+        System.out.println(change_id);
+    }
+}
